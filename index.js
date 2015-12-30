72a1c4e2241bef8e28f60f592425fbe46d6461f9
MiGatoSeneca <pablo@citious.com>
2015-12-30 15:50:00 +0100

    update

--- index.js
+++ index.js
@@ -75,10 +75,10 @@ io.on('connection', function (socket) {
 
   // when the client emits 'add user', this listens and executes
   socket.on('add user', function (username) {
+
     if (addedUser) return;
 
-
-
+    socket.receiver="brand.abanca";
     // we store the username in the socket session for this client
     socket.sender = username;
 
