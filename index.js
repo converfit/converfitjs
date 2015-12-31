08b4ae7aac49765784fe346590f5fd36e46bc033
MiGatoSeneca <pablo@citious.com>
2015-12-31 08:11:46 +0100

    Update

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ io.on('connection', function (socket) {
 
     users[socket.id]=socket.username;
 
+    users.each(function(value, key) {
+      console.log("["+key+"] "+value);
+    });
+
     ++numUsers;
     addedUser = true;
     socket.emit('login', {
