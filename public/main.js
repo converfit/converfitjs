c36f650c48c7e815a0ae20e6675b5d1295d907c6
MiGatoSeneca <pablo@citious.com>
2016-01-04 12:04:49 +0100

    Update

--- public/main.js
+++ public/main.js
@@ -25,7 +25,7 @@ $(function() {
   var lastTypingTime;
   var $currentInput = $usernameInput.focus();
 
-  var socket = io();
+  var socket = io('converfit.com/node/');
 
   if (typeof localStorage["username"] != 'undefined'){
     login(localStorage["username"]);
