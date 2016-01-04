37d611be4ddf0606cf98c6e07f7f86846803f1b0
MiGatoSeneca <pablo@citious.com>
2016-01-04 11:56:07 +0100

    Update

--- app.js
+++ app.js
@@ -44,12 +44,7 @@ app.get('/logout',function(req, res){
 // Routing
 
 
-//app.use(express.static(__dirname + '/public'));
-
-app.get(function(req, res, next){
-  console.log("inside");
-  next();
-});
+app.use(express.static(__dirname + '/public'));
 
 /*
 app.use(function(req, res) {
