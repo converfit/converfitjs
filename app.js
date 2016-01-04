ecdc7a51f59fa4add494d5b4bc3ab87cad3f32e4
MiGatoSeneca <pablo@citious.com>
2016-01-04 14:43:06 +0100

    Update

--- app.js
+++ app.js
@@ -41,139 +41,139 @@ var users = {};
 
 io.on('connection', function (socket) {
   var addedUser = false;
+  if(typeof receiver.username == 'undefined'){
 
-  var queryString = 'SELECT * FROM users WHERE username="'+receiver.username+'"';
-  console.log("[MySQL] "+queryString);
-  db.query(queryString,function(err, rows, fields) {
-      if (err) throw err;
-      if (rows==0){
-        res.sendFile(__dirname + '/404/index.html');
-      }else{
-        socket.receiver=rows[0].username;
-        socket.header=rows[0].header;
-        socket.emit('user header',socket.header);
-      }
-  });
-
-
-  socket.on('login', function (username){
-
-    var queryString = 'SELECT * FROM users WHERE username="/user/'+username+'"';
+    var queryString = 'SELECT * FROM users WHERE username="'+receiver.username+'"';
     console.log("[MySQL] "+queryString);
-    db.query(queryString, function(err, rows, fields) {
+    db.query(queryString,function(err, rows, fields) {
         if (err) throw err;
         if (rows==0){
-          socket.emit('login error');
+          res.sendFile(__dirname + '/404/index.html');
         }else{
-          socket.sender = '/user/'+username;
-          users[socket.id]=username;
-          ++numUsers;
-          addedUser = true;
-
-          var queryString = 'SELECT * FROM messages WHERE owner="'+socket.sender+'" and (sender="'+socket.receiver+'" or receiver="'+socket.receiver+'")';
-          console.log("[MySQL] "+queryString);
-          db.query(queryString,function(err, rows, fields) {
-              if (err) throw err;
-              if (rows!=0){
-                socket.emit('messages backup', rows);
-              }
-          });
-
-          socket.emit('logged', users);
-
-          socket.broadcast.emit('user joined', {
-            username: socket.username,
-            socketid: socket.id
-          });
+          socket.receiver=rows[0].username;
+          socket.header=rows[0].header;
+          socket.emit('user header',socket.header);
         }
     });
-  });
+    socket.on('login', function (username){
 
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
+      var queryString = 'SELECT * FROM users WHERE username="/user/'+username+'"';
+      console.log("[MySQL] "+queryString);
+      db.query(queryString, function(err, rows, fields) {
+          if (err) throw err;
+          if (rows==0){
+            socket.emit('login error');
+          }else{
+            socket.sender = '/user/'+username;
+            users[socket.id]=username;
+            ++numUsers;
+            addedUser = true;
 
-    var message ={
-      owner:socket.sender,
-      sender:socket.sender,
-      receiver:socket.receiver,
-      type:"chat",
-      body:data,
-      unread:"0"
-    };
+            var queryString = 'SELECT * FROM messages WHERE owner="'+socket.sender+'" and (sender="'+socket.receiver+'" or receiver="'+socket.receiver+'")';
+            console.log("[MySQL] "+queryString);
+            db.query(queryString,function(err, rows, fields) {
+                if (err) throw err;
+                if (rows!=0){
+                  socket.emit('messages backup', rows);
+                }
+            });
 
-    db.query('INSERT INTO messages SET ?', message, function(err,res){
-      if(err) throw err;
+            socket.emit('logged', users);
+
+            socket.broadcast.emit('user joined', {
+              username: socket.username,
+              socketid: socket.id
+            });
+          }
+      });
     });
 
-    var message ={
-      owner:socket.receiver,
-      sender:socket.sender,
-      receiver:socket.receiver,
-      type:"chat",
-      body:data,
-      unread:"1"
-    };
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', function (data) {
 
-    db.query('INSERT INTO messages SET ?', message, function(err,res){
-      if(err) throw err;
+      var message ={
+        owner:socket.sender,
+        sender:socket.sender,
+        receiver:socket.receiver,
+        type:"chat",
+        body:data,
+        unread:"0"
+      };
+
+      db.query('INSERT INTO messages SET ?', message, function(err,res){
+        if(err) throw err;
+      });
+
+      var message ={
+        owner:socket.receiver,
+        sender:socket.sender,
+        receiver:socket.receiver,
+        type:"chat",
+        body:data,
+        unread:"1"
+      };
+
+      db.query('INSERT INTO messages SET ?', message, function(err,res){
+        if(err) throw err;
+      });
+
+
+      // we tell the client to execute 'new message'
+      socket.broadcast.emit('new message', {
+        sender: socket.username,
+        body: data
+      });
     });
 
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (username) {
+      if (addedUser) return;
 
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      sender: socket.username,
-      body: data
-    });
-  });
+      // we store the username in the socket session for this client
+      socket.username = username;
 
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-
-    users[socket.id]=socket.username;
+      users[socket.id]=socket.username;
 
 
 
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', users);
+      ++numUsers;
+      addedUser = true;
+      socket.emit('login', users);
 
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      socketid: socket.id
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
-    if (addedUser) {
-      --numUsers;
-
-      console.log("Socket disconnect "+socket.id);
-
-      delete users[socket.id]
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
+      socket.broadcast.emit('user joined', {
         username: socket.username,
         socketid: socket.id
       });
-    }
-  });
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function () {
+      socket.broadcast.emit('typing', {
+        username: socket.username
+      });
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', function () {
+      socket.broadcast.emit('stop typing', {
+        username: socket.username
+      });
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function () {
+      if (addedUser) {
+        --numUsers;
+
+        console.log("Socket disconnect "+socket.id);
+
+        delete users[socket.id]
+        // echo globally that this client has left
+        socket.broadcast.emit('user left', {
+          username: socket.username,
+          socketid: socket.id
+        });
+      }
+    });
+  }
 });
