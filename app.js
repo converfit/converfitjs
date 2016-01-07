952c15d0dad98d264281fbce41c2c1c4f68bd409
MiGatoSeneca <pablo@citious.com>
2016-01-07 16:55:21 +0100

    update

--- app.js
+++ app.js
@@ -70,7 +70,10 @@ io.on('connection', function (socket) {
       var queryString = 'SELECT * FROM users WHERE username="'+username+'"';
       console.log("[MySQL] "+queryString);
       db.query(queryString, function(err, rows, fields) {
-          if (err) throw err;
+          if (err) {
+            console.log("[socket.emit] login error");
+            socket.emit('login_error',1);
+          }
           if (rows==0){
             console.log("[socket.emit] login error");
             socket.emit('login_error',1);
